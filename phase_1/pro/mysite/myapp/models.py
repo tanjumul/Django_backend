5e8a20ff3cecca179a850f756a2be90fbe6a2dee
2019331101-Tanjumul <crackerroot4@gmail.com>
2026-01-03 19:53:03 +0600

    initializing the model structure for db

--- phase_1/pro/mysite/myapp/models.py
+++ phase_1/pro/mysite/myapp/models.py
@@ -6,6 +6,6 @@ class Items(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField()
     item_price = models.IntegerField()
-    
+    item_disc = models.IntegerField()
    
     
