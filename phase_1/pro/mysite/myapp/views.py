30ff48cfcf262fe3a1d05c015da90838de6f6759
2019331101-Tanjumul <crackerroot4@gmail.com>
2026-01-04 00:13:43 +0600

    rendering db values into landing app view

--- phase_1/pro/mysite/myapp/views.py
+++ phase_1/pro/mysite/myapp/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Item
 
 # Create your views here.
 
 def index(response):
-    return HttpResponse("Daily Blog Page : Tanjumul")
+    item_lists = Item.objects.all()
+    return HttpResponse(item_lists)
 
 
 def item(request):
