3c3b25d9c93174e323387090d98bf34f86c2dea6
2019331101-Tanjumul <crackerroot4@gmail.com>
2026-01-03 21:27:37 +0600

    string representation in  terminal we change in modes with self

--- phase_1/pro/mysite/myapp/models.py
+++ phase_1/pro/mysite/myapp/models.py
@@ -3,6 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Items(models.Model):
+    
+    def __str__(self): 
+        return self.item_name
+    
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField()
     item_price = models.IntegerField()
