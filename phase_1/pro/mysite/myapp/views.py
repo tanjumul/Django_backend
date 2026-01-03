573a3f831600490092c02a54007ea0233ea608aa
2019331101-Tanjumul <crackerroot4@gmail.com>
2026-01-04 01:25:58 +0600

    making an item list and adding to templete section

--- phase_1/pro/mysite/myapp/views.py
+++ phase_1/pro/mysite/myapp/views.py
@@ -5,9 +5,12 @@ from . models import Item
 # Create your views here.
 
 def index(response):
-    item_lists = Item.objects.all()
-    return HttpResponse(item_lists)
-
+    item_list = Item.objects.all()
+    context ={ 
+              'item_list': item_list
+              }
+    # return HttpResponse(item_lists)
+    return render(response,"myapp/index.html",context)
 
 def item(request):
     return HttpResponse('<h1>This is an it section</h1>')
