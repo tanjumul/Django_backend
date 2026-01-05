ba925335acadde1afff9c5c71543c3ef2c885b2a
2019331101-Tanjumul <crackerroot4@gmail.com>
2026-01-06 00:28:05 +0600

    image field creating and rendering in the html file

--- phase_1/pro/mysite/myapp/models.py
+++ phase_1/pro/mysite/myapp/models.py
@@ -11,6 +11,6 @@ class Item(models.Model):
     item_desc = models.CharField()
     item_price = models.IntegerField()
     item_disc = models.IntegerField()
-   
+    item_image = models.CharField(max_length=500,default='https://www.foodservicerewards.com/cdn/shop/t/262/assets/fsr-placeholder.png?v=45093109498714503231652397781')
     #you may need to migrate the server to initialize the whold structure to your settings's installed apps to initialize the dbs
     
